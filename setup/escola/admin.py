64cc173379b447d79cae2902bb6187a63a0b47c1
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-03-10 16:26:06 +0000

    Add matricula

--- setup/escola/admin.py
+++ setup/escola/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 class Alunos(admin.ModelAdmin):
     list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
@@ -13,5 +13,11 @@ class Cursos(admin.ModelAdmin):
     list_display_links = ('id', 'codigo')
     search_fields = ('codigo_curso',)
 
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'periodo')
+    list_display_links = ('id',)
+
 admin.site.register(Aluno, Alunos)
 admin.site.register(Curso, Cursos)
+admin.site.register(Matricula, Matriculas)
--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from escola.views import AlunosViewSet, CursosViewSet
+from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
 router.register('cursos', CursosViewSet, basename='Cursos')
+router.register('matriculas', MatriculasViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
