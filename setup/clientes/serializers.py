81455e0c47237e3d43c0aa61fc614a6cde222c0b
Mauricio Calheiro <mauriciocalheiro95@gmail.com>
2024-03-10 17:50:00 +0000

    Add other validations

--- setup/clientes/serializers.py
+++ setup/clientes/serializers.py
@@ -6,7 +6,22 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf(self, cpf):
+    def validate_nome(self, nome:str) -> str:
+        if not nome.isalpha():
+            raise serializers.ValidationError('O nome nao deve ter digitos')
+        return nome
+
+    def validate_cpf(self, cpf:str) -> str:
         if len(cpf) != 11:
             raise serializers.ValidationError('O CPF deve ter 11 digitos')
         return cpf
+
+    def validate_rg(self, rg:str) -> str:
+        if len(rg) != 9:
+            raise serializers.ValidationError('O RG deve ter 9 digitos')
+        return rg
+    
+    def validate_celular(self, celular:str) -> str:
+        if len(celular) < 11:
+            raise serializers.ValidationError('O celular deve ter pelo menos 11 digitos')
+        return celular
